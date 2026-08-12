8e6bacfa3e51d6b0046ba428571b538ff7afbe79
OLIVIA SANTOS NUNES <25162445@edu119.local>
2026-08-12 15:41:05 -0300

    Ajuste do readme

---
+++ lib/main.dart
@@ -0,0 +1,278 @@
+
+
+import 'package:flutter/material.dart'; //Importar mtodos os componentes do Flutter
+
+void main() {
+  runApp(const MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  const MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true
+      ),
+      home: MediaEscolarPage()
+    );
+  }
+}
+
+class MediaEscolarPage extends StatefulWidget{
+  const MediaEscolarPage({super.key});
+
+  @override
+  State<MediaEscolarPage> createState () => _MediaEscolarPageState();
+  }
+
+class _MediaEscolarPageState extends State<MediaEscolarPage>{
+  
+  final TextEditingController nomeController = TextEditingController();
+  final TextEditingController nota1Controller = TextEditingController();
+  final TextEditingController nota2Controller = TextEditingController();
+  final TextEditingController nota3Controller = TextEditingController();
+  final TextEditingController nota4Controller = TextEditingController();
+
+  String nomeAluno= '';
+  String situacao='';
+  double media = 0;
+
+  void calcularMedia(){
+    String nome = nomeController.text.trim();
+
+    double? nota1 = double.tryParse(
+        nota1Controller.text.replaceAll(",", "."),
+    );
+    double? nota2 = double.tryParse(
+        nota2Controller.text.replaceAll(",", "."),
+    );
+    double? nota3 = double.tryParse(
+        nota3Controller.text.replaceAll(",", "."),
+    );
+    double? nota4 = double.tryParse(
+        nota4Controller.text.replaceAll(",", "."),
+    );
+
+    if(
+      nome.isEmpty || nota1 == null || nota2 == null || nota3 == null || nota4 == null
+      ){
+        mostrarMensagem('Preencha todos os campos corretamente');
+        return;
+    }
+
+    if(
+      nota1 < 0  ||nota1 > 10 || nota2 < 0  || nota2 > 10 || nota3 < 0 || nota4 > 10 || nota4 < 0 || nota4 > 10 
+    ){
+      mostrarMensagem('As notas devem estar entre 0 e 10.');
+      return;
+    }
+
+    double mediaCalculada = (nota1 + nota2 + nota3 + nota4) /4;
+
+    String situacaoCalculada;
+
+    if(mediaCalculada >= 7){
+      situacaoCalculada = 'APROVADO';
+    }else if(mediaCalculada >= 5){
+      situacaoCalculada = 'RECUPERAÇÃO';
+    }else{
+      situacaoCalculada = 'REPROVADO';
+    }
+
+    setState(() {
+      nomeAluno = nome;
+      media = mediaCalculada;
+      situacao = situacaoCalculada;
+    });
+
+
+  }
+
+  void mostrarMensagem(String mensagem){
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(mensagem)
+        ) ,
+    );
+  }
+
+  void limpar(){
+    nomeController.clear();
+    nota1Controller.clear();
+    nota2Controller.clear();
+    nota3Controller.clear();
+    nota4Controller.clear();
+
+    setState(() {
+      nomeAluno = '';
+      media = 0;
+      situacao = '';
+    });
+  }
+
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Calculadora de Média"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Icon(Icons.school,size: 80),   
+
+            const SizedBox(height:10),     
+
+            const Text(
+              'Média Escolar',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+
+            const SizedBox(height: 5,),
+
+            const Text(
+                'Digite o nome e as três notas do aluno',
+                textAlign: TextAlign.center,
+            ),
+
+            const SizedBox(height: 25,),
+
+            TextField(
+                controller: nomeController,
+                decoration: const InputDecoration(
+                  labelText: 'Nome do aluno',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.person),
+                  hintText: 'Exemplo: Henry'
+                ),
+            ),
+
+            const SizedBox(height: 15,),
+
+            TextField(
+                controller: nota1Controller,
+                decoration: const InputDecoration(
+                  labelText: 'Nota 1',
+                  hintText: 'Digite uma nota de 0 a 10',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.edit),
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true
+                ),
+            ) ,
+
+             const SizedBox(height: 15,),
+
+            TextField(
+                controller: nota2Controller,
+                decoration: const InputDecoration(
+                  labelText: 'Nota 2',
+                  hintText: 'Digite uma nota de 0 a 10',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.edit),
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true
+                ),
+            ) ,
+
+             const SizedBox(height: 15,),
+
+            TextField(
+                controller: nota3Controller,
+                decoration: const InputDecoration(
+                  labelText: 'Nota 3',
+                  hintText: 'Digite uma nota de 0 a 10',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.edit),
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true
+                ),
+            ) ,
+             const SizedBox(height: 15,),
+
+            TextField(
+                controller: nota4Controller,
+                decoration: const InputDecoration(
+                  labelText: 'Nota 4',
+                  hintText: 'Digite uma nota de 0 a 10',
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.edit),
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true
+                ),
+            ) ,
+
+            const SizedBox(height: 20,),
+
+            ElevatedButton.icon(
+              onPressed: calcularMedia, 
+              icon: const Icon(Icons.calculate),
+              label: const Text('Calcular média')
+            ),
+
+            const SizedBox(height: 15,),
+
+            OutlinedButton.icon(
+              onPressed: limpar,
+              icon: const Icon(Icons.clear),
+              label: const Text('Limpar')
+            ),
+
+            const SizedBox(height: 25,),
+
+            if(situacao.isNotEmpty)
+              Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Column(
+                    children: [
+                      Text(
+                        nomeAluno,
+                        style: const TextStyle(
+                          fontSize: 22,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+                      Text(
+                        'Média: ${media.toStringAsFixed(1)}',
+                        style: const TextStyle(
+                          fontSize: 20
+                        ),
+                      ),
+                      const SizedBox(height: 10,),
+
+                      Text(
+                        situacao,
+                        style: const TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+
+          ],
+        ),
+        
+      ),
+    );
+  }
+}
---
+++ lib/readme.md
@@ -0,0 +1,54 @@
+# Atividade - Calculadora de Média Escolar
+
+### Objetivo
+
+- MaterialApp
+- Scaffold
+- StateFulWidget
+- TextField
+- TextEditingController
+- ElevateButton
+- OutlineButton
+- setState
+- Variáveis
+- Conversão de texto para número
+- Condicionais
+- Funções
+- Validação de campos
+- SnackBar
+
+### Funcionamento 
+
+O usuário deverá informar: 
+- Nome do aluno;
+- Primeira nota;
+- Segunda nota;
+- Terceira nota;
+
+O sistema deverá calcular a média e mostrar:
+- Aprovado: média maior ou igual a 7;
+- Recuperação: média maior ou igual 5 e menor que 7;
+- Reprovado: média menor que 5;
+
+### Checklist
+- [X] Criar projeto Flutter;
+- [X] Criar campo de nome;
+- [X] Criar os três campos de nota;
+- [x] Criar o botão de calcular;
+- [x] Calcular a média;
+- [x] Verificar a situação do aluno;
+- [x] Mostrar o resultado;
+- [x] Criar o botão limpar;
+- [x] Validar nota entre 0 e 10;
+- [x] Testar o aplicativo;
+
+### Desafio
+
+- [X] Adicionar uma quarta nota
+- [ ] Mostrar a maior nota
+- [ ] Mostrar a menor nota
+- [ ] Informar quantos pontos faltaram para aprovação
+- [ ] Adicionar um campo para frequência
+- [ ] Reprovar o aluno que tiver frequência menor que 75%
+
+
